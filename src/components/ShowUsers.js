07dc13ccc64cc29977b6869e328575ed958f6b15
dqmdz <daniel.quinterospinto@gmail.com>
2023-11-24 05:00:55 -0300

    Agregado de tabla de usuarios

--- src/components/ShowUsers.js
+++ src/components/ShowUsers.js
@@ -16,13 +16,60 @@ const ShowUsers = () => {
         getUsers();
     }, []);
 
-    const getUsers = async() => {
+    const getUsers = async () => {
         const response = await axios.get(url);
         setUsers(response.data);
     }
 
     return (
-        <div>ShowUsers</div>
+        <div className='App'>
+            <div className='container-fluid'>
+                <div className='row mt-3'>
+                    <div className='col-md-4-offset-md-4'>
+                        <div className='d-grid mx-auto'>
+                            <button className='btn btn-dark' data-bs-toggle='modal' data-bs-target='#modalUsers'>
+                                <i className='fa-solid fa-circle-plus'></i> Añadir
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='row-mt-3'>
+                    <div className='col-12 col-lg-8 offset-0 offset-lg-2'>
+                        <div className='table-responsive'>
+                            <table className='table table-bordered'>
+                                <thead>
+                                    <tr><th>#</th><th>First Name</th><th>Last Name</th><th>Active</th><th></th></tr>
+                                </thead>
+                                <tbody className='table-group-divider'>
+                                    {
+                                        users.map((user, i) => (
+                                            <tr key={user.id}>
+                                                <td>{(i + 1)}</td>
+                                                <td>{user.firstname}</td>
+                                                <td>{user.lastname}</td>
+                                                <td>{user.active ? 'Sí' : 'No'}</td>
+                                                <td>
+                                                    <button className='btn btn-warning'>
+                                                        <i className='fa-solid fa-edit'></i>
+                                                    </button>
+                                                    &nbsp;
+                                                    <button className='btn btn-danger'>
+                                                        <i className='fa-solid fa-trash'></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                        )
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='modal-fade'>
+            </div>
+        </div>
     )
 }
 
