4266eecbde92225311e0427a8dffac87fa24aa28
JuanIgnacioKristich <j.kristich@etec.local>
2023-11-28 10:16:00 -0300

    martes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       </Routes>
 
       <Routes>
-        <Route path='/products' element={<ShowProducts></ShowProducts>}>
+        <Route path='/api/products' element={<ShowProducts></ShowProducts>}>
         </Route>
       </Routes>
 
